0c69854d8060962461572e0186e1ca88fd890536
GaneshKumar <ganesh.kumar@smart.aerial.machines.com>
2018-08-24 16:57:59 +0200

    Adding sam_ins project

---
+++ sam_ins/src/ins_node.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include "ServerClass.h"
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <string>
+#include <sstream>
+using namespace std;
+int cnt;
+string cnt_str;
+string file;
+char buffer[1024];
+void sig_handler(int sig);
+
+bool ctrl_c_pressed = false;
+ros::Time tnow, tprev;
+ros::Duration dur;
+
+stringstream ss;
+ofstream ccfile;
+std_msgs::String ins_msg;
+
+int main(int argc, char **argv)
+{
+
+    struct sigaction sig_struct;
+    sig_struct.sa_handler = sig_handler;
+    sig_struct.sa_flags = 0;
+    sigemptyset(&sig_struct.sa_mask);
+    ccfile.open("ContextCapture.txt");
+
+    if (sigaction(SIGINT, &sig_struct, NULL) == -1) {
+        cout << "Problem with sigaction" << endl;
+        exit(1);
+    }
+    //Ros related calls
+    ros::init(argc, argv, "ins_node");
+    ros::NodeHandle nh;
+    //IMU string publisher
+    ros::Publisher ins_pub = nh.advertise<std_msgs::String>("/ins_node/ins_msg", 10);
+       
+    // UDP socket realted
+    //char buffer[1024];
+    Server_UDP udp_obj;
+    bool bflag = udp_obj.Server_start();
+    bool recvflag = true;
+    ros::Rate rate(100);
+    tprev = ros::Time::now();
+    while(ros::ok())
+    {
+        tnow = ros::Time::now();
+	dur = tnow - tprev;
+	tprev = tnow;
+
+	recvflag = udp_obj.Server_recv(buffer, 1024);
+	ss << buffer;
+        ins_msg.data = ss.str();
+	if(recvflag)
+	{
+	    ins_pub.publish(ins_msg);
+
+	}
+	else
+	{ 
+	    cout << "Buffer not received";
+	}
+
+        if(ctrl_c_pressed)
+        {
+            cout << "Ctrl^C Pressed" << endl;
+            udp_obj.~Server_UDP();
+            break;
+
+         }
+         rate.sleep();
+	 ros::spinOnce();
+	 ss.str(std::string());
+	 cout << "Time period: " << dur << endl;
+    }
+    cout << "Exiting....." << endl;
+    return 0;
+}
+
+void sig_handler(int sig)
+{
+    write(0,"nCtrl^C pressed in sig handlern",32);
+    ctrl_c_pressed = true;
+}
